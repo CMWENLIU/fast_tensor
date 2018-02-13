c00e02248dbf60ba008338f412acbb876ed53f69
cmwenliu <cmwenliu@gmail.com>
2018-02-13 17:04:05 -0600

    .

--- exp/tensor_test.py
+++ exp/tensor_test.py
@@ -8,8 +8,8 @@ x = 2
 y = 3
 op1 = tf.add(x, y)
 op2 = tf.multiply(x, y)
-op3 = tf.pow(op2, op3)
-with tf.Session as sess:
+op3 = tf.pow(op1, op2)
+with tf.Session() as sess:
 	op3 = sess.run(op3)
 print (op3)
 '''
