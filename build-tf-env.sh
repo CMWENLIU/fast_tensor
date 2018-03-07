27d30bd6518438bf28092fe683e1ab38e2580348
cmwenliu <cmwenliu@gmail.com>
2018-03-07 09:14:10 -0600

    .

---
+++ build-tf-env.sh
@@ -0,0 +1,20 @@
+cd ~
+# You can change what anaconda version you want at 
+# https://repo.continuum.io/archive/
+wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
+bash Anaconda3-5.0.1-Linux-x86_64.sh -b -p ~/anaconda
+rm Anaconda3-5.0.1-Linux-x86_64.sh
+echo 'export PATH="~/anaconda/bin:$PATH"' >> ~/.bashrc 
+
+# Refresh basically
+source ~/.bashrc
+
+conda update conda
+# Refresh for updating
+source ~/.bashrc
+
+# Install tf-gpu v1.3
+conda install -c jjhelmus tensorflow-gpu-base 
+# Refresh for tensorflow
+source ~/.bashrc
+done
