c9c36c17ca74f7702e1e87781652d7b0b93421d9
Aven Xiangwen Liu <CMWENLIU@GMAIL.COM>
2018-06-21 21:56:40 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,10 +6,6 @@ import tensorflow as tf
 eps = 1e-12
 pp = pprint.PrettyPrinter()
 
-try:
-    xrange
-except NameError:
-    xrange = range
 
 def progress(progress):
     barLength = 20 # Modify this to change the length of the progress bar
@@ -45,6 +41,16 @@ def gather(m_or_v, idx):
         return m_or_v
 
 def argmax(x):
+    index = 0
+    max_num = x[index]
+    for idx in xrange(1, len(x)-1):
+        if x[idx] > max_num:
+            index = idx
+            max_num = x[idx]
+    return index, max_num
+
+
+def argmax2(x):
     index = 0
     max_num = x[index]
     for idx in xrange(1, len(x)-1):
